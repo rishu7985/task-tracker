9f527caaa5b3368b827f942f710ef95729cc9a65
Rishabh Yadav <rishabhy13579@gmail.com>
2026-01-07 01:57:21 +0530

    Move main.cpp into src folder

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Task Tracker CLI\n";
+    cout << "Commands:\n";
+    cout << "  add <task>\n";
+    cout << "  list\n";
+    cout << "  done <id>\n";
+    return 0;
+}
